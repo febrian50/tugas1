0db51845aad8d5bcdf2c778d30888a71126e8bba
Febrian Agung <48670014+febrian50@users.noreply.github.com>
2019-04-29 02:05:03 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+
+<h1>Hello World</h1> <br/><table>
+    <tr>
+        <td>NIM</td>
+        <td> : </td>
+        <td>16.01.53.0140</td>
+    </tr>
+    <tr>
+        <td>Nama</td>
+        <td> : </td>
+        <td>OKTIO RYAN ARDIYANTO</td>
+    </tr>
+</table>
+<a href="/unisbank/unisbank">Go To Akademik</a>
